011cd745099a80e768697daa3cd92e468056176e
s16217@pjwstk.edu.pl <#Pjwstk888>
2021-04-25 10:15:55 +0200

    Add Charts

--- app/src/main/java/com/example/kalkulatorbmi/DeathActivity.java
+++ app/src/main/java/com/example/kalkulatorbmi/DeathActivity.java
@@ -12,16 +12,44 @@ import android.widget.TextView;
 import android.widget.EditText;
 import android.widget.Button;
 
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Pie;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class DeathActivity extends AppCompatActivity {
 
+    AnyChartView anyChartView;
+
+    String[] months = {"Jan","Feb","Mar"};
+    int[] earnings = {12000, 8009, 10000};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+
+                super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_death);
-        WebView webView=findViewById(R.id.webview);
-        webView.loadUrl("file:///android_asset/index.html");
+
+        anyChartView = findViewById(R.id.any_chart_view);
+
+        setupPieChart();
+    }
+
+    public void setupPieChart() {
+        Pie pie = AnyChart.pie();
+        List<DataEntry> dataEntries = new ArrayList<>();
+
+        for (int i = 0; i < months.length; i++){
+            dataEntries.add(new ValueDataEntry(months[i], earnings[i]));
+        }
+
+        pie.data(dataEntries);
+        anyChartView.setChart(pie);
 
     }
 }
